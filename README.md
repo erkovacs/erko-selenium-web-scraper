3968a364251702a0c3b9b4c6e4782ad251aba3e4
erkovacs/erko/ekovacs <kovaroberik@gmail.com>
2021-04-24 12:33:21 -0700

    added options

--- README.md
+++ README.md
@@ -16,5 +16,11 @@
  - Run `python3 scraper.py`
  - Results will appear in `./results/` as a JSON file
 
+### Options
+ - `-n` - Do not launch headless Chrome. Used for debugging mostly
+ - `-p` - Pagination limit (i.e. how many pages of results should it scrape)
+ - `-l` - Result limit (i.e. how many total results to scrape)
+ - `-r` - Preserve raw HTML in the data (dramatically increases size but preserves markup for further usage)
+
 ## Acknowledgements
  - [This StackOverflow post](https://stackoverflow.com/questions/1936466/beautifulsoup-grab-visible-webpage-text)
--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import time
 import json
+import getopt
+import sys
 from urllib.parse import urlparse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -24,13 +26,13 @@ def write_file (file_name, file_contents):
         if not (file_handle is None):
             file_handle.close()
 
-def parse (result, html, raw=False):
+def parse (result, html, raw):
     if raw:
         result['rawHtml'] = html
 
     result['textContent'] = rip_text(html)
 
-def search (driver, where, what, pages=1, limit=1999):
+def search (driver, where, what, pages, limit, save_raw):
     documents = []
     
     # search for what on site where
@@ -75,7 +77,7 @@ def search (driver, where, what, pages=1, limit=1999):
         
         # get html
         html = driver.page_source
-        parse(result, html)
+        parse(result, html, save_raw)
 
         # go back
         driver.back()
@@ -89,12 +91,12 @@ def search (driver, where, what, pages=1, limit=1999):
 def get_domain (url):
     return urlparse(url).netloc
 
-def main ():
+def main (headless, pages, limit, save_raw):
     search_sites = get_cfg('./config/search_sites.cfg')
     search_terms = get_cfg('./config/search_terms.cfg')
 
     options = Options()
-    options.headless = True
+    options.headless = headless
     driver = webdriver.Chrome(options=options)
     
     site_results = {}
@@ -103,7 +105,7 @@ def main ():
         site_results[domain] = []
 
         for term in search_terms:
-            results = search(driver, site, term, pages=1, limit=10)
+            results = search(driver, site, term, pages, limit, save_raw)
             for result in results:
                 site_results[domain].append(result)
         
@@ -112,4 +114,26 @@ def main ():
             
     driver.quit()
 
-main()
+headless = True
+pages = 1
+limit = 1999
+save_raw = False
+
+opts = getopt.getopt(sys.argv[1:], 'np:l:r')
+
+for k, v in opts[0]:
+    if k == '-n': # not headless
+        headless = False
+    elif k == '-p': # page limit
+        pages = int(v)
+    elif k == '-l': # result limit
+        limit = int(v)
+    elif k == '-r': # save raw html too
+        save_raw = True
+    else:
+        print(f'Unknown option {k}. Ignored.')
+
+main(headless,
+    pages,
+    limit,
+    save_raw)
